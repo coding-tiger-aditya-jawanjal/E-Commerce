361928e1610af97c4c8562f91248fb6232b92753
Coding Tiger <coding.tiger.mail@gmail.com>
2024-06-27 18:29:28 +0530

    Footer

---
+++ client/src/seed/data.js
@@ -0,0 +1,19 @@
+export const FooterData = [
+	{
+		title: "Company",
+		features: [
+			"About Us",
+			"Our Privacy",
+			"Privacy Policy",
+			"Affiliate Program",
+		],
+	},
+	{
+		title: "Get Help",
+		features: ["FAQ", "Shipping", "Returns", "Order Status", "Payment Options"],
+	},
+	{
+		title: "Online Shop",
+		features: ["Watch", "Bag", "Shoes", "Dress"],
+	},
+];
